392273f2c67bdd9c9235305a7008c2ace63a012f
Tkariclook <tkariclook@gmail.com>
2021-01-09 17:22:41 +0300

    t26MergeTwoSortedLists

---
+++ src/main/scala/difficultyLevels/easy/t26MergeTwoSortedLists/solution.scala
@@ -0,0 +1,19 @@
+package difficultyLevels.easy.t26MergeTwoSortedLists
+
+object solution extends App {
+  def mergeTwoLists(l1: ListNode, l2: ListNode): ListNode = {
+    (l1, l2) match {
+      case (null, null) => l1
+      case (b, _) if b == null => l2
+      case (_, a) if a == null => l1
+      case _ if l1.x < l2.x => new ListNode(l1.x, mergeTwoLists(l1.next, l2))
+      case _ if l1.x >= l2.x => new ListNode(l2.x, mergeTwoLists(l1, l2.next))
+    }
+  }
+
+  val l11 = new ListNode(1, new ListNode(2, new ListNode(4)))
+  val l22 = new ListNode(1, new ListNode(3, new ListNode(4)))
+  val res0 = mergeTwoLists(l11,l22)
+  println(res0)
+  val res = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(4)))))
+}
