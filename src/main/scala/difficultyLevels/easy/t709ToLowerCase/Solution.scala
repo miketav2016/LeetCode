9a85b40a1f8a48ae62cc5ee84ccc1d7b0934f47e
Tkariclook <tkariclook@gmail.com>
2021-02-13 18:20:27 +0300

    t709ToLowerCase

---
+++ src/main/scala/difficultyLevels/easy/t709ToLowerCase/Solution.scala
@@ -0,0 +1,10 @@
+package difficultyLevels.easy.t709ToLowerCase
+
+/**
+ * Implement function ToLowerCase() that has a string parameter str, and returns the same string in lowercase.
+ */
+object Solution extends App {
+  def toLowerCase(str: String): String = {
+    str.toLowerCase
+  }
+}
