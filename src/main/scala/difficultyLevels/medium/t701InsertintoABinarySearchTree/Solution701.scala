16d854f1dbcb31b437f6639cd32cfedc251f1b70
Tkariclook <tkariclook@gmail.com>
2022-01-12 21:09:25 +0300

    t701InsertIntoABinarySearchTree

---
+++ src/main/scala/difficultyLevels/medium/t701InsertintoABinarySearchTree/Solution701.scala
@@ -0,0 +1,22 @@
+package difficultyLevels.medium.t701InsertintoABinarySearchTree
+
+
+
+// Definition for a binary tree node.
+class TreeNode(_value: Int = 0, _left: TreeNode = null, _right: TreeNode = null) {
+       var value: Int = _value
+       var left: TreeNode = _left
+       var right: TreeNode = _right
+     }
+
+class Solution701 {
+    def insertIntoBST(root: TreeNode, `val`: Int): TreeNode = {
+
+        if (root == null) return new Nothing(`val`)
+        if (`val` > root.value)
+            root.right = insertIntoBST(root.right, `val`) // Continue searching in the right subtree
+        else
+            root.left = insertIntoBST(root.left, `val`) // Continue searching in the left subtree
+        root
+    }
+}
